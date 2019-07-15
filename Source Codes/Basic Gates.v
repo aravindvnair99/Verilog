be4e003741da9756acb6a19940b3315208167828
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-07-15 10:15:21 +0530

    Update Basic Gates.v

--- Source Codes/Basic Gates.v
+++ Source Codes/Basic Gates.v
@@ -1,8 +1,7 @@
-module Basic_Gates(a,b,c,out);
+module Basic_Gates(a,b,c,and_out,or_out,not_out);
 	input a,b,c;
-	output out;
-	wire out,w1,w2;
-	and (w1,a,b,c);
-	not (w2,c);
-	or (out,w1,w2);
+	output and_out,or_out,not_out;
+	and (and_out,a,b);
+	not (not_out,c);
+	or (or_out,a,b);
 endmodule
