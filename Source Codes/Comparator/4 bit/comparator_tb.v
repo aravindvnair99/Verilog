2ce66660d50603e0648a6ddeb2c501fb91e9b4da
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-10-14 09:17:08 +0530

    Create comparator_tb.v

---
+++ Source Codes/Comparator/4 bit/comparator_tb.v
@@ -0,0 +1,34 @@
+module comparator_tb;
+
+	// Inputs
+	reg [3:0] A;
+	reg [3:0] B;
+
+    // Outputs
+	wire less;
+	wire equal;
+	wire greater;
+
+	// Instantiate the Unit Under Test (UUT)
+	comparator uut (
+		.A(A), 
+		.B(B), 
+		.less(less), 
+		.equal(equal), 
+		.greater(greater)
+	);
+
+	initial begin
+		//Apply inputs
+		A = 10;
+		B = 12;
+		#100;
+		A = 15;
+		B = 11;
+		#100;
+		A = 10;
+		B = 10;
+		#100;
+	end
+  
+endmodule
